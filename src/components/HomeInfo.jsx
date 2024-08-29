713b5b3ef233aeaee7d0b8c73bbfccffaf7bf0bc
Harvey Vilchez <80428143+Kal-024@users.noreply.github.com>
2024-08-29 17:11:45 -0600

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -6,11 +6,11 @@ const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Adrian</span>
+        Hola, Soy
+        <span className='font-semibold mx-2 text-white'>Harvey</span>
         👋
         <br />
-        A Software Engineer from Nicaragua 🇭🇷
+        Ingeniero de Software from Nicaragua 🇳🇮
       </h1>
     );
 
